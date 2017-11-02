bccdaff884f2f63bb00dec11900dff2bf73bb344
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-11-02 09:55:36 +0100

    rwere

---
+++ repeat_strategy/promises/example1.js
@@ -0,0 +1,35 @@
+/* ES5, using Bluebird */
+
+var isMomHappy = true;
+//Promise
+var willIGetNewPhone = new Promise(
+  function (resolve,reject){
+    if(isMomHappy){
+      var phone = {
+        brande:"samsung",
+        color:"black"
+      };
+      resolve(phone);
+    }else {
+      var reason = new Error('mom is not happy');
+      reject(reason);
+    }
+  }
+);
+// here we call our promise
+// call our promise
+
+
+var askMom = function () {
+    willIGetNewPhone
+        .then(function (fulfilled) {
+            // yay, you got a new phone
+            console.log(fulfilled);
+        })
+        .catch(function (error) {
+            // ops, mom don't buy it
+            console.log(error.message);
+        });
+}
+
+askMom();
