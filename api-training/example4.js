06527a7d5353eca3bcb75debe5f781c999d2c82d
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-11-02 12:40:34 +0100

    tet

--- api-training/example4.js
+++ api-training/example4.js
@@ -1,3 +1,34 @@
+/*
+Write a function 'transformFirstAndLast' that takes in an array, and returns an object with:
+1) the first element of the array as the object's key, and
+2) the last element of the array as that key's value.
+Example input:
+['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce']
+Function's return value (output):
+{
+  Queen : 'Beyonce'
+}
+Do not change the input array. Assume all elements in the input array will be of type 'string'.
+Note that the input array may have a varying number of elements. Your code should flexibly accommodate that.
+E.g. it should handle input like:
+['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey']
+Function's return value (output):
+{
+  Kevin : 'Spacey'
+}
+Starter Code
+function transformFirstAndLast(array) {
+  // your code here
+}
+*/
+
+/*function transformFirstAndLast(array) {
+  //your code here
+ var test=['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey'];
+  console.log(test[0]+" "+ test[5]);
+}
+transformFirstAndLast();*/
+
 function transformFirstAndLast(array) {
 
   var myObject = {};
---
+++ api-training/example5.js
@@ -0,0 +1,25 @@
+/*
+Write a function 'fromListToObject' which takes in an array of arrays, and returns an object with each pair of elements in the array as a key-value pair.
+
+Example input:
+[['make', 'Ford'], ['model', 'Mustang'], ['year', 1964]]
+
+Function's return value (output):
+{
+  make : 'Ford'
+  model : 'Mustang',
+  year : 1964
+}
+
+Do not change the input string. Assume that all elements in the array will be of type 'string'.
+
+Note that the input may have a different number of elements than the given sample.
+For instance, if the input had 6 values instead of 4, your code should flexibly accommodate that.
+
+Starter Code:
+*/
+
+function fromListToObject(array) {
+  // your code here
+
+}
