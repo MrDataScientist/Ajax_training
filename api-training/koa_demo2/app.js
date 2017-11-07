88442dce7c8782bdf1cac315a138541cbf7fcb1a
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-11-07 10:58:23 +0100

    reg

--- api-training/koa_demo2/app.js
+++ api-training/koa_demo2/app.js
@@ -1,10 +1,14 @@
 var koa = require('koa');
+var router = require('koa-router');
 var app = new koa();
 
-app.use(function* (){
-   this.body = 'Hello world!';
-});
+var _ = router(); //Instantiate the router
 
-app.listen(3000, function(){
-   console.log('Server running on https://localhost:3000')
-});
+_.get('/hello', getMessage); // Define routes
+
+function *getMessage() {
+   this.body = "Hello world!";
+};
+
+app.use(_.routes()); //Use the routes defined using the router
+app.listen(3000);
