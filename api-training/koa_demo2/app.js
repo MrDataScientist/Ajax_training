c478cfd934465f41100401f5887fc91b566a1337
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-11-07 10:52:15 +0100

    ztru

--- api-training/koa_demo2/app.js
+++ api-training/koa_demo2/app.js
@@ -1,15 +1,15 @@
 var koa = require('koa');
 var router = require('koa-router');
-var app = koa();
+var app = new koa();
 
-var _ = router();
+var test = router();
 
-_.get('/hello', getMessage);
+test.get('./hello', getMessage);
 
 function *getMessage(){
    console.log(this.request);
    this.body = 'Your request has been logged.';
 }
 
-app.use(_.routes());
+app.use(test.routes());
 app.listen(3000);
