547df222a4024842a965defc79d8904754451f12
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-11-22 16:09:19 +0100

    r344tw

--- ethereum_tutos/func-wallet-master/src/components/SendTokens.vue
+++ ethereum_tutos/func-wallet-master/src/components/SendTokens.vue
@@ -29,7 +29,7 @@
         <v-list-tile>
           <v-spacer></v-spacer>
           <v-list-tile-action>
-            <v-btn primary dark>Send</v-btn>
+            <v-btn primary dark @click.native="sendTokens">Send</v-btn>
           </v-list-tile-action>
         </v-list-tile>
 
@@ -42,6 +42,7 @@
 
 //import config from '../config'
 import { CONTRACT }  from '../contract'
+import _ from 'lodash'
 
 export default {
   data () {
@@ -54,10 +55,22 @@ export default {
   methods: {
     sendTokens(){
       if(!web3.isAddress(this.addr)){
-        alert('Invalid address!')
-        this.addr = null
-        return
+        alert('Invalid address!');
+        this.addr = null;
+        return;
       }
+
+      if(!_.isNumber(this.amount) || this.amount <= 0){
+        alert('Invalid amount!');
+        this.amount = null;
+        return;
+      }
+      CONTRACT.transfert(this.addr, this.amount, (err, res) => {
+        if(!err){
+          console.log(res)
+        }
+        console.log(res)
+      })
     }
   }
 }
--- ethereum_tutos/func-wallet-master/src/config.js
+++ ethereum_tutos/func-wallet-master/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  contractAddress: '0x186fdf9deb16f8feb2e4b65793bf7e45b4facad3',
+  contractAddress: '0xded8566db750d9f8ed4999f1dad3039b987a8a20',
   contractAbi: [{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
   {"constant":true,"inputs":[{"name":"_owner","type":"address"}],
   "name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],
