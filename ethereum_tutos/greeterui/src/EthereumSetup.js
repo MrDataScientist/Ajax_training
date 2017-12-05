24348ac0856cad12d9b890848b92aec9468d5dcc
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-12-05 16:36:48 +0100

    ger

---
+++ ethereum_tutos/greeterui/src/EthereumSetup.js
@@ -0,0 +1,7 @@
+import web3 from 'web3';
+const web3 = new web3(new web3.providers.HttpProvider("https://localhost:8545"));
+let greeterABI = [{}];
+let greeterAddress = '';
+const greeterContract = web3.eth.contract(greeterABI).at(greeterAddress);
+
+export(greeterContract)
