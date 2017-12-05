db759fe8410cafd95d3ff05c5c453a22a6b3b037
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-12-05 16:55:27 +0100

    edfwe

--- ethereum_tutos/greeterui/src/App.js
+++ ethereum_tutos/greeterui/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      greeting:""
+    }
+  }
+
   render() {
     return (
       <div className="App">
