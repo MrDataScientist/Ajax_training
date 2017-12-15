f3947486ad70e78e613f061963ba53ab35e7a064
Tarik EN NAKDI <t.ennakdi@gmail.com>
2017-12-15 10:03:25 +0100

    tzutz

---
+++ ethereum_tutos/Customer.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.18;
+
+contract Customer {
+
+   string fName;
+   uint age;
+   address owner;
+
+   function Owned() public{
+       owner = msg.sender;
+   }
+
+   modifier onlyOwner{
+       require(msg.sender == owner);
+       _;
+   }
+
+    event Customer(
+       string name,
+       uint age
+    );
+
+   function setCustomer(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+       Customer(_fName, _age);
+   }
+
+   function getCustomer() view public returns (string, uint) {
+       return (fName, age);
+   }
+
+}
