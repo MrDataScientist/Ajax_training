64e71a2ea39a69b58548055865f20868fea6b917
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-01-02 14:50:11 +0100

    erferg

--- datatables/vue-cli/src/Home.vue
+++ datatables/vue-cli/src/Home.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <p>Server status: {{ status }}</p>
-    <hr>
-    <button @click="changeStatus">Change Status</button>
+    <app-server-status v-for="server in 5"></app-server-status>
   </div>
 
 </template>
 
 <script>
+  import ServerStatus from './ServerStatus.vue';
   export default{
-    data: function(){
-      return {
-        status: 'Critical'}
-    },
-    methods: {
-      changeStatus(){
-        this.status = 'Normal';
-      }
-
+    components: {
+      'app-server-status': ServerStatus
     }
   }
 </script>
