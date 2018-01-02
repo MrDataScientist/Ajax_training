d2020f92b3e5097f3ad131bc3fc9b06b0f196575
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-01-02 15:50:21 +0100

    dewfwe

--- datatables/Component_udemy_courses/Section Code (Start)/src/App.vue
+++ datatables/Component_udemy_courses/Section Code (Start)/src/App.vue
@@ -1,19 +1,26 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Components</h1>
-
+            <div class="col-xs-12">
+                <app-user></app-user>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import User from './components/User.vue';
+
     export default {
+        components: {
+            appUser: User
+        }
     }
 </script>
 
 <style>
-
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
