7fd1efcda76747a67d687ba9911573118e3c6b0c
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-01-04 15:11:59 +0100

    egte

--- datatables/vue-resource/Section Code (Start)/src/main.js
+++ datatables/vue-resource/Section Code (Start)/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
 import VueResource from 'vue-resource';
+import App from './App.vue'
 
-vue.use(VueResource);
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
