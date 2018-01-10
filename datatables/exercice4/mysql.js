90322edd172fa9f92f84fceac91d20ab9dbd064f
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-01-10 17:19:48 +0100

    dffs

---
+++ datatables/exercice4/mysql.js
@@ -0,0 +1,16 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'cloudeo'
+});
+
+connection.connect();
+
+connection.query('SELECT * AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
