ec27ccea117451f321bcb61f297322b8af043e27
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-01 15:15:03 +0100

    regerger

--- 25october/Section Code (Start)/src/routes.js
+++ 25october/Section Code (Start)/src/routes.js
@@ -1,8 +1,9 @@
 import Home from ':/components/Home.vue';
-import
+import Portfolio from './components/portfolio/Portfolio.vue';
+import Stocks from './components/stocks/Stocks.vue';
 
 export const routes = [
     {path: '/', component: Home},
-    {path: '/portfolio', component:Portfolio},
-    {path: '/stocks', component: stocks}
+    {path: '/portfolio', component: Portfolio},
+    {path: '/stocks', component: Stocks}
 ];
