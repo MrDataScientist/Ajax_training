268b5acdd8628572ec67321639f24c5777bb349e
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-01 15:12:44 +0100

    refergfer

---
+++ 25october/Section Code (Start)/src/routes.js
@@ -0,0 +1,8 @@
+import Home from ':/components/Home.vue';
+import
+
+export const routes = [
+    {path: '/', component: Home},
+    {path: '/portfolio', component:Portfolio},
+    {path: '/stocks', component: stocks}
+];
