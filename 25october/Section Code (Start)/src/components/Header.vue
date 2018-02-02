ea4d2bbd1dbfeee6ad5b34ca5d004f34fac9fd44
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-02 15:58:52 +0100

    rgerh

--- 25october/Section Code (Start)/src/components/Header.vue
+++ 25october/Section Code (Start)/src/components/Header.vue
@@ -17,9 +17,12 @@
                            data-toggle="dropdown"
                            role="button"
                            aria-haspopup="true"
-                           aria-expanded="false"> <span class="caret"></span></a>
+                           aria-expanded="false"> Save & Load <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li> <a href="">Save Data</a></li>
+                        </ul>
 
-                    </li>
+                    </ul>
 
                 </ul>
             </div><!-- /.navbar-collapse -->
