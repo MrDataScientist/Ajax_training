54fff8f4f110e801c8abe62f4088bc6a5f4fcae6
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-02 15:35:51 +0100

    fwe

--- 25october/Section Code (Start)/src/components/Header.vue
+++ 25october/Section Code (Start)/src/components/Header.vue
@@ -1,15 +1,10 @@
 <template>
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Brand</a>
+
+                <router-link>Stock Trader</router-link>
+
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
