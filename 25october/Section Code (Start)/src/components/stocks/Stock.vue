0843c7dd897bb21de42ca686ccebc988ebf9b429
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-02 17:35:29 +0100

    gerger

---
+++ 25october/Section Code (Start)/src/components/stocks/Stock.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-sm-6 col-md-4">
+        <div class="panel panel-success">
+            <div class="panel-deading">
+                <h3 class="panel-title">
+                    NAME
+                    <small> (Price: PRICE)</small>
+                </h3>
+            </div>
+            <div class="panel-body">
+                <div class="pull-left">
+                    <input
+                        type="number"
+                        class="form-control"
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- 25october/Section Code (Start)/src/components/stocks/stocks.vue
+++ 25october/Section Code (Start)/src/components/stocks/stocks.vue
@@ -1,3 +1,21 @@
 <template>
     <h1>The stocks Component</h1>
+    <div>
+
+    </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return{
+                stocks:[
+                    {id:1, name:'BMW', price:983},
+                    {id:2, name:'Ford', price:367},
+                    {id:3, name:'Mercedes', price:200},
+                    {id:4, name:'BMW', price:145}
+                ]
+            }
+        }
+    }
+</script>
