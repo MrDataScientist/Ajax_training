9a9ed62968c36a9ec3a1cee2009dcc2134670272
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-02 10:17:21 +0100

    grthrt

--- 25october/Section Code (Start)/src/App.vue
+++ 25october/Section Code (Start)/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <router-view></router-view>
 
     </div>
 </template>
--- 25october/Section Code (Start)/src/main.js
+++ 25october/Section Code (Start)/src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 
 import App from './App.vue'
-import {routes}
+import {routes} from './routes';
 
 Vue.use(VueRouter);
 
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
 new Vue({
   el: '#app',
+    router,
   render: h => h(App)
 })
