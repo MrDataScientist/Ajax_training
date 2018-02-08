5a6a5d9dd1d3d295ff2189124eac96799db2d9fa
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-02-08 17:04:55 +0100

    gergerger

--- 25october/Section Code (Start)/src/components/stocks/Stock.vue
+++ 25october/Section Code (Start)/src/components/stocks/Stock.vue
@@ -8,13 +8,6 @@
                 </h3>
             </div>
             <div class="panel-body">
-                <div class="pull-left">
-                    <input
-                        type="number"
-                        class="form-control"
-                        placeholder="Quantity"
-                    >
-                </div>
 
                 <div class="pull-left">
                     <input
@@ -23,11 +16,12 @@
                             placeholder="Quantity"
                             v-model="quantity"
                     >
+
                     <div class="pull-right">
                         <button
                                 class="btn btn-success"
                                 @click="buyStock"
-                                :disabled="quantity <=0"
+
                         >Buy</button>
                              <!--   :disabled="quantity <=0"  -->
 
