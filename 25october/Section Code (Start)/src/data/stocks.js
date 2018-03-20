56a6bc73435c31ba4fb49e72c5215037a41d8960
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-03-20 14:50:26 +0100

    z45z56

--- 25october/Section Code (Start)/src/data/stocks.js
+++ 25october/Section Code (Start)/src/data/stocks.js
@@ -1,6 +1,6 @@
-export default { // export a default array
-    {   id:1, name:'BMW', price: 983},
-    {   id:2, name:'Ford', price: 367},
-    {   id:3, name:'Mercedes', price: 200},
-    {   id:4, name:'BMW', price: 145}
-};
+export default [ // export a default array
+{ id:1, name:'BMW', price:983},
+{ id:2, name:'Ford', price:367},
+{ id:3, name:'Mercedes', price:200},
+{ id:4, name:'BMW', price: 145}
+];
--- 25october/Section Code (Start)/src/store/store.js
+++ 25october/Section Code (Start)/src/store/store.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import stocks from './modules/stocks';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
