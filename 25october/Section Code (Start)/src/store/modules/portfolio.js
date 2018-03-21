e0b7d149a0033188f7e651b1e3c00756f29fc1a9
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-03-21 10:58:09 +0100

    rfrferfer

--- 25october/Section Code (Start)/src/store/modules/portfolio.js
+++ 25october/Section Code (Start)/src/store/modules/portfolio.js
@@ -35,6 +35,18 @@ const actions = {
 
 const getters = {
     stockPortfolio (state, getters){
-        return state.stocks
+        return state.stocks.map(stock => {
+            const record = getters.stocks.find(element => element.id == stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        });
+    },
+    funds (state) {
+        return state.funds;
     }
 };
+
