1d2dcd3036ab695c25870c9885fada5073aa8292
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-03-21 10:15:52 +0100

    frferg

--- 25october/Section Code (Start)/src/components/Blockchain/Blocks.vue
+++ 25october/Section Code (Start)/src/components/Blockchain/Blocks.vue
@@ -1,7 +1,4 @@
 <template>
     <h1>The Blockchain Component</h1>
-    <h1>MetaMask Tip Button Example</h1>
-    <a href="https://github.com/MetaMask/TipButton">View on Github</a>
-    <div class="tip-button"></div>
-    <div class="message"></div>
+
 </template>
