6e17d9c354193b76ec331ab804d0f555cdf2bc57
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-03-22 15:04:30 +0100

    treget

--- Section Code (Finished)/src/App.vue
+++ Section Code (Finished)/src/App.vue
@@ -1,45 +1,59 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-xs-12">
-                <button @click="selectedComponent = 'appQuote'">Quote</button>
-                <button @click="selectedComponent = 'appAuthor'">Author</button>
-                <button @click="selectedComponent = 'appNew'">New</button>
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Routing</h1>
                 <hr>
-                <p>{{ selectedComponent }}</p>
-                <keep-alive>
-                    <component :is="selectedComponent">
-                        <p>Default Content</p>
-                    </component>
-                </keep-alive>
-                <!--<app-quote>-->
-                    <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
-                    <!--<p>A wonderful Quote</p>-->
-                <!--</app-quote>-->
+                <router-view name="header-top"></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+                <router-view name="header-bottom"></router-view>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Quote from './components/Quote.vue';
-    import Author from './components/Author.vue';
-    import New from './components/New.vue';
-
+    import Header from './components/Header.vue';
     export default {
-        data: function() {
-            return {
-                quoteTitle: 'The Quote',
-                selectedComponent: 'appQuote'
-            }
-        },
         components: {
-            appQuote: Quote,
-            appAuthor: Author,
-            appNew: New
+            appHeader: Header
         }
     }
 </script>
 
 <style>
+    .slide-leave-active {
+        transition: opacity 1s ease;
+        opacity: 0;
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    .slide-leave {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    @keyframes slide-out {
+        0% {
+            transform: translateY(0);
+        }
+        100% {
+            transform: translateY(-30px);
+        }
+    }
+
+    @keyframes slide-in {
+        0% {
+            transform: translateY(-30px);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
 </style>
