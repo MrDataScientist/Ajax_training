207f161182a28e2557e9e7ab95cde92f17b1a67b
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-03 14:47:56 +0200

    45z4

---
+++ etherPayment.js
@@ -0,0 +1,43 @@
+function myFunction() {
+
+  var price = Drupal.settings.ether.price;
+  var orderId = Drupal.settings.ether.orderId;
+  var url = Drupal.settings.ether.url;
+  var randomKey = Drupal.settings.ether.randomKey;
+  var MY_ADDRESS = '0xfE7Fb0105f070a17E8fE210e33F0bE034Da1599D';
+
+  if (typeof web3 === 'undefined') {
+    return renderMessage('<div>You need to install <a href=“https://metmask.io“>MetaMask </a> to use this feature.  <a href=“https://metmask.io“>https://metamask.io</a></div>')
+  }
+
+  var user_address = web3.eth.accounts[0]
+
+  web3.eth.sendTransaction({
+      to: MY_ADDRESS,
+      from: user_address,
+      value: web3.toWei(price, 'ether'),
+    },
+
+    function (err, transactionHash) {
+      if (err) return renderMessage('The payment could not be completed')
+
+      // If you get a transactionHash, you can assume it was sent,
+      // or if you want to guarantee it was received, you can poll
+      // for that transaction to be mined first.
+
+      renderMessage('The payment has been successfully submitted')
+    })
+
+  function renderMessage(message) {
+    var messageEl = document.querySelector(".message")
+    messageEl.innerHTML = message
+  }
+}
+
+function sendFeedback(){
+  var price = Drupal.settings.ether.price;
+  var orderId = Drupal.settings.ether.orderId;
+  var url = Drupal.settings.ether.url;
+  var randomKey = Drupal.settings.ether.randomKey;
+  windows.location ="receive.php?price"+ price + "orderId"+ orderId + "url" + url + "randomkey" + randomkey;
+}
