1ea624d627b0ef8a1b0364452548aca6afb659db
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-18 14:59:48 +0200

    6

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -2,6 +2,10 @@ const koa = require('koa');
 const app = new koa();
 const PORT = 4000;
 
+app.use(async ctx =>{
+  ctx.body = 'Hello World';
+})
+
 app.listen(PORT);
 
 // to show which port our server is running on
