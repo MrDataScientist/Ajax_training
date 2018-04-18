19919419f1e004fba3bf2cfd8f8f3dbd97a6d112
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-18 15:51:09 +0200

    regfer

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -2,6 +2,16 @@ const koa = require('koa');
 const app = new koa();
 const PORT = 4000;
 
+app.use(async (ctx, next) =>{
+  console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
+  return await next();
+})
+
+app.use(async (ctx, next) =>{
+  console.log();
+  return await next();
+})
+
 app.use(async ctx =>{
   ctx.body = 'Hello World';
 })
