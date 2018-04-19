2a5e120487173dfd47b5be0ee1ad69b22411574a
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-19 11:43:17 +0200

    routers endpoints

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -7,10 +7,35 @@ const PORT = 4000;
 
 // create a root route
 const router = new Router();
+
+// posts json dummy data
+const posts = [
+    {
+        "id" : '1',
+        "name" : "Node developer",
+        "content" : "you should read this I"
+    },
+    {
+        "id" : '2',
+        "name" : "frontend developer",
+        "content" : "you should read this II"
+    },
+    {
+        "id" : '3',
+        "name" : "Ethereum developer",
+        "content" : "you should read this III"
+    }
+]
+
 // welcome to koa application
 router.get('/', (ctx, next) => {
     ctx.body = 'Welcome to koa application';
 });
+
+// create a new router
+router.get('/posts', ctx => {
+    ctx.body = posts;
+})
 
 app
     .use(router.routes())
