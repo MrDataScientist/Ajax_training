c798c3c6b8c029d9fefb6fa2e9114890811f09aa
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-19 10:00:52 +0200

    uzizu

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -1,5 +1,3 @@
-const _ = require('koa-route');
-
 const koa = require('koa');
 const app = new koa();
 const PORT = 4000;
@@ -8,29 +6,6 @@ const PORT = 4000;
 // create a root route
 // welcome to koa application
 
-
-const db = {
-    tobi: { name: 'tobi', species: 'ferret' },
-    loki: { name: 'loki', species: 'ferret' },
-    jane: { name: 'jane', species: 'ferret' }
-};
-
-const pets = {
-    list: (ctx) => {
-        const names = Object.keys(db);
-        ctx.body = 'pets: ' + names.join(', ');
-    },
-
-    show: (ctx, name) => {
-        const pet = db[name];
-        if (!pet) return ctx.throw('cannot find that pet', 404);
-        ctx.body = pet.name + ' is a ' + pet.species;
-    }
-};
-
-
-app.use(_.get('/pets', pets.list));
-app.use(_.get('/pets/:name', pets.show));
 
 
 
