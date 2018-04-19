30e227619ba3fc905c5d289a9f50c8d7ec3ec6b5
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-19 09:49:04 +0200

    trhetztzj

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -1,7 +1,38 @@
+const _ = require('koa-route');
 const koa = require('koa');
 const app = new koa();
 const PORT = 4000;
 
+// import the koa-router
+// create a root route
+// welcome to koa application
+
+
+const db = {
+    tobi: { name: 'tobi', species: 'ferret' },
+    loki: { name: 'loki', species: 'ferret' },
+    jane: { name: 'jane', species: 'ferret' }
+};
+
+const pets = {
+    list: (ctx) => {
+        const names = Object.keys(db);
+        ctx.body = 'pets: ' + names.join(', ');
+    },
+
+    show: (ctx, name) => {
+        const pet = db[name];
+        if (!pet) return ctx.throw('cannot find that pet', 404);
+        ctx.body = pet.name + ' is a ' + pet.species;
+    }
+};
+
+
+app.use(_.get('/pets', pets.list));
+app.use(_.get('/pets/:name', pets.show));
+
+
+/*
 app.use(async (ctx, next) =>{
   console.log(`${ctx.method} ${ctx.url} ${new Date()}`);
   return await next();
@@ -15,6 +46,7 @@ app.use(async (ctx, next) =>{
 app.use(async ctx =>{
   ctx.body = 'Hello World';
 })
+*/
 
 app.listen(PORT);
 
