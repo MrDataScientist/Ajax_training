4f78d4514c3728cb449d9818e5e3d8ee48cff44a
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-19 10:09:52 +0200

    rege

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -1,13 +1,21 @@
 const koa = require('koa');
+// import the koa-router
+const Router = require('koa-router');
+
 const app = new koa();
 const PORT = 4000;
 
-// import the koa-router
 // create a root route
+const router = new Router();
 // welcome to koa application
+router.get('/', (ctx, next) => {
+    console.log('Welcome to Koa a middleware javascript framework!');
+    return next();
+});
 
-
-
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 app.listen(PORT);
 // to show which port our server is running on
