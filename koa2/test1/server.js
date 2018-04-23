c0c9ea6237ae54f8c0a2f4e0750b2c29480e3d6c
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-04-23 15:21:30 +0200

    commit 9

--- koa2/test1/server.js
+++ koa2/test1/server.js
@@ -10,19 +10,19 @@ app.use(bodyParser());
 
 const posts = [
     {
-        "id" : '1',
-        "name" : "Node developer",
-        "content" : "you should read this I"
+        "SKU" : "1",
+        "price" : "10",
+        "orderID" : "11"
     },
     {
-        "id" : '2',
-        "name" : "frontend developer",
-        "content" : "you should read this II"
+        "SKU" : "2",
+        "price" : "20",
+        "orderID" : "12"
     },
     {
-        "id" : '3',
-        "name" : "Ethereum developer",
-        "content" : "you should read this III"
+        "SKU" : "3",
+        "price" : "30",
+        "orderID" : "13"
     }
 ];
 
@@ -32,11 +32,11 @@ router.get('/', ctx => {
 
 router.post('/posts', ctx => {
     console.log(ctx.request.body);
-    let {id, name, content} = ctx.request.body;
-    if(!id){ctx.threw(400, 'id is required field')}
-    if(!name){ctx.threw(400, 'name is required field')}
-    if(!content){ctx.threw(400, 'content is required field')}
-    posts.push({id, name, content});
+    let {SKU, price, orderID} = ctx.request.body;
+    if(!SKU){ctx.threw(400, 'SKU is required field')}
+    if(!price){ctx.threw(400, 'price is required field')}
+    if(!orderID){ctx.threw(400, 'orderID is required field')}
+    posts.push({SKU, price, orderID});
     ctx.body = posts;
 })
 
