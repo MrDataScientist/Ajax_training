c326edc001ca80907c11c18fabedb4702a7a5e1a
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-05-02 15:40:22 +0200

    commit 10

--- koa2/koa-sequelize/models/index.js
+++ koa2/koa-sequelize/models/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+let fs        = require('fs');
+let path      = require('path');
+let Sequelize = require('sequelize');
+let basename  = path.basename(__filename);
+let env       = process.env.NODE_ENV || 'development';
 let config    = require(__dirname + '/../config/config.json')[env];
 let db        = {};
 
@@ -20,7 +20,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    let model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
