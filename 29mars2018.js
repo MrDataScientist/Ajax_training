0e158d6a11e46d22a51ab84be421742c909bf2e9
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-05-04 15:06:34 +0200

    Update 29mars2018.js

--- 29mars2018.js
+++ 29mars2018.js
@@ -1,7 +1,6 @@
 /*
 ### web3js is an Ethereum Javascript Api
 ### source: https://github.com/ethereum/web3.js/
-
 ### After installation, you can use this function to call MetaMask
 */
 // ***********this function call Metamask to make a payment transafert using ether Tokens**********************************************************
