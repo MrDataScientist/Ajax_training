98ac5c5ae227e9c5842102e78098ef0324b2991d
Tarik EN NAKDI <t.ennakdi@gmail.com>
2018-05-17 17:05:44 +0200

    cmmit 1

--- koa-casts-master/models/application.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var Application = sequelize.define('Application', {
-    status_id: DataTypes.INTEGER
-  });
-  return Application;
-};
